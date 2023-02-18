5e61193d9579246c6feca59fe2637fd79d781140
jimliu01 <jim1394783013@gmail.com>
2023-02-17 23:02:22 -0800

    complete

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import sklearn as sk
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
+import os
+import pandas as pd
+import sqlite3
 
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -11,7 +14,6 @@ from matplotlib.figure import Figure
 import io
 import base64
 
-### stuff from last class
 app = Flask(__name__)
 
 @app.route('/')
@@ -24,81 +26,74 @@ def ask():
         return render_template('ask.html')
     else:
         try:
-            return render_template('ask.html', name=request.form['name'], student=request.form['student'])
+            insert_message(request)
+            return render_template('ask.html', name=request.form['name'], message=request.form['message'])
         except:
             return render_template('ask.html')
-#######
 
-####### group exercise
-@app.route('/hello/')
-def hello():
-    return render_template('hello.html')
+@app.route('/view/',methods=["GET"])
+def view():
 
-@app.route('/hello/<name>')
-def hello_name(name):
-    return render_template('hello.html', name=name)
+    msgs=random_messages(2)
+    return render_template('hello.html',msgs=msgs)
 
-#######
-# Request object: https://flask.palletsprojects.com/en/2.1.x/api/#flask.Request
-@app.route('/submit-basic/', methods=['POST', 'GET'])
-def submit_basic():
-    if request.method == 'GET':
-        return render_template('submit-basic.html')
-    else:
-        try:
-            # this is how you can access the uploaded file
-            # img = request.files['image']
-            return render_template('submit-basic.html', thanks=True)
-        except:
-            return render_template('submit-basic.html', error=True)
+def get_message_db():
+# return the message database, and if its first time, then create a table and return it
+  try:
+          return g.message_db
+  except:
+          g.message_db = sqlite3.connect("messages_db.sqlite")
+          cmd = """
+          CREATE TABLE
+          IF NOT EXISTS message(
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            name text, 
+            message text) 
+          """ # replace this with your SQL query
+          cursor = g.message_db.cursor()
+          cursor.execute(cmd)
+          return g.message_db
 
 
-# matplotlib: https://matplotlib.org/3.5.0/gallery/user_interfaces/web_application_server_sgskip.html
-# plotly: https://towardsdatascience.com/web-visualization-with-plotly-and-flask-3660abf9c946
-@app.route('/submit-advanced/', methods=['POST', 'GET'])
-def submit():
-    if request.method == 'GET':
-        return render_template('submit.html')
-    else:
-        try:            
-            '''
-            1. Access the image
-            2. Load the pickled ML model
-            3. Run the ML model on the image
-            4. Store the ML model's prediction in some Python variable
-            5. Show the image on the template
-            6. Print the prediction and some message on the template
-            '''
-            # 1
-            img = request.files['image'] # file object 144.txt 
-            img = np.loadtxt(img) # numpy array with the pixel values
 
-            x = img.reshape(1, 64)
-            
-            # 2
-            model = pickle.load(open('mnist-model/model.pkl', 'rb'))
-            
-            # 3, 4
-            digit = model.predict(x)[0]
+def insert_message(request):
+    # inser the message into the database by committing
+    conn= get_message_db()
+    name= request.form.get("name",'')
+    message= request.form.get("message",'')
+    cmd = f"""
+    INSERT INTO message values(null,'{name}','{msg}')
+    """ 
+    cursor = conn.cursor()
+    cursor.execute(cmd)
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return 
 
-            # 5 
-            fig = Figure(figsize=(3, 3))
-            ax = fig.add_subplot(1, 1, 1,)
-            ax.imshow(img, cmap='binary')
-            ax.axis("off")
 
-            # weird part of 5
-            pngImage = io.BytesIO()
-            FigureCanvas(fig).print_png(pngImage) # convert the pyplot figure object to a PNG image
+def random_messages(n=2):
+    # this is where we output
+    out=[]
+    # sql command, n can be changed according to input
+    cmd=f"SELECT * FROM message ORDER BY RANDOM() LIMIT {n}"
 
-            # encode the PNG image to base64 string
-            pngImageB64String = "data:image/png;base64,"
-            pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
+    for i in range(n):
+        conn=get_message_db()
+        cursor=conn.cursor()
+        cursor.execute(cmd)
+        fetch=cursor.fetchone()
+        out.append(fetch)
 
-            return render_template('submit.html',
-             image=pngImageB64String, digit=digit)
-        except:
-            return render_template('submit.html', error=True)
+    cursor.close()
+    conn.close()
+    return out
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    get_message_db()
